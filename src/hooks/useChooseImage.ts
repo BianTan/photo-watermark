cc6cf874155972ae28fc679351c4b9fd8911e204
BianTan <biantan@outlook.com>
2023-02-08 14:30:33 +0800

    🐞 fix: wip

--- src/hooks/useChooseImage.ts
+++ src/hooks/useChooseImage.ts
@@ -1,24 +1,17 @@
 
 const input = document.createElement('input')
 input.type = 'file'
-let isClick = false
 
 export function useChooseImage(accept = 'image/*'): Promise<File | undefined> {
   return new Promise((resolve) => {
-    if (isClick) return undefined
-    isClick = true
   
     input.accept = accept
     input.click()
 
     input.onchange = (event: Event) => {
       const file = (event.target as HTMLInputElement).files?.[0]
-      isClick = false
       input.value = ''
       resolve(file)
-    }
-    input.oncancel = () => {
-      isClick = false
     }
   })
 }
